f96bb21edb54230f18b2fdcd4ed1f156e890bbd1
Ivanoherlihy <117639027+Ivanoherlihy@users.noreply.github.com>
2022-11-17 11:56:20 +0000

    Add files via upload

--- json_passing.py
+++ json_passing.py
@@ -1,83 +1,62 @@
 import json
-# parser class??
-
-'''exports a dict to json with a given filename'''
-
 
 def json_export(location, dictOut):
-    outfile = open(location, "w")
+    outfile = open(location,"w")
+
 
     outfile.write(json.dumps(dictOut))
     outfile.close
 
-
-''' Reads the entire dictionary from json'''
 def json_read(location):
-    infile = open(location, "r")
+    infile = open(location,"r")
     x = infile.read()
     infile.close()
-    # loads json into dict
     return json.loads(x)
 
-
-''' Updates a single key, value pair. id is the key of the outer dictionary, 
-    key is the key, value pair in the nested dict'''
-
-
-def json_updater(location, id, key, value):
-    infile = open(location, "r+")
-    # db is a python dict
+def json_updater(location,id, key, value):
+    infile = open(location,"r+")
     db = json.loads(infile.read())
     infile.close
-    # assigns the changed
-    changedItem = db[str(id)]
-    # update the value of the changed item
+
+    changedItem =db[str(id)]
     changedItem[key] = value
-    outfile = open(location, "w")
-    # dumps the dict into json
+    outfile = open(location,"w")
     outfile.write(json.dumps(db))
     outfile.close()
 
 
-''' Helper function to simplify the ... changes id back to int.'''
 def read_bus():
-    temp = json_read("bus.json")
+    temp =  json_read("bus.json")
     returnDict = {}
-    for count, value in enumerate(temp):
-        returnDict[count] = temp[value]
+    for count, value in enumerate(temp):   
+        returnDict[count] = temp[value]    
     return returnDict
+    
 
-''' Helper '''
 def export_bus(dictOut):
-    json_export("bus.json", dictOut)
+    json_export("bus.json" ,dictOut)
 
+def update_bus(id,key,value):
+    json_updater("bus.json",id,key,value)
 
-''' Helper function for updating buses'''
-def update_bus(id, key, value):
-    json_updater("bus.json", id, key, value)
-
-''' Getter for particular buses as dict'''
 def get_bus(id):
     buses = read_bus()
-    return buses[str(id)]
+    return buses[id]
 
-''' Helper funciton for changeing route_id back to int'''
 def read_routes():
-    temp = json_read("routes.json")
+    temp =  json_read("routes.json")
     returnDict = {}
-    for count, value in enumerate(temp):
+    for count, value in enumerate(temp):   
         returnDict[count] = temp[value]
     return returnDict
 
-''' overwrites entire routes.json file'''
 def export_routes(dictOut):
-    json_export("routes.json", dictOut)
+    json_export("routes.json",dictOut)
 
-''' updates entire routes.json'''
-def update_routes(id, key, value):
-    json_updater("routes.json", id, key, value)
+def update_routes(id,key,value):
+    json_updater("routes.json",id,key,value)
 
-''' return single route as a dict'''
 def get_route(id):
     routes = read_routes()
-    return routes[str(id)]
+    return routes[id]
+
--- setup.py
+++ setup.py
@@ -1,30 +1,30 @@
 import json_passing
 from random import randint
-
-''' Sets the number of buses to 'Parked' 
-id's start at 0'''
 def setup_bus(num_buses):
     outDict = {}
     for i in range(num_buses):
         temp = {}
         temp["charge"] = 100
-        temp["endOfJourney"] = False
+        temp["lastJourney"] = False
         temp["status"] = "parked"
         temp["routeID"] = None
+        
+
         busID = i
         outDict[busID] = temp
     json_passing.export_bus(outDict)
 
-''' sets up random values for testing '''
-def setup_random_routes(num_routes):
+
+def setup_radom_routes(num_routes):
     outDict = {}
     for i in range(num_routes):
         temp = {}
-        temp["A_to_B"] = randint(5,20)
-        temp["B_to_A"] = randint(5,20)
-        temp["A_to_Depot"] = randint(0,15)
-        temp["B_to_Depot"] = randint(0,15)
+        temp["A-B"] = randint(5,20)
+        temp["B-A"] = randint(5,20)
+        temp["A-Depot"] = randint(0,15)
+        temp["B-Depot"] = randint(0,15)
         routeID = i
         outDict[routeID] = temp
     json_passing.export_routes(outDict)
 
+setup_radom_routes(1)
