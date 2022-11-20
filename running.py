e2d8e240d612fd9c774f384df036d1dd6d4a3184
Colin <108523746@umail.ucc.ie>
2022-11-20 14:49:22 +0000

    Refactored running class

--- running.py
+++ running.py
@@ -14,14 +14,14 @@ for i in range(5):
 #passes id of the next bus with status parked
 def getAvaliableBus():
     for bus in buses:
-        if bus.getStatus() == "parked":
+        if bus.get_status() == "parked":
             return bus
 
 #sets the next bus that is parked to a particulat route in a particular direction
 def setNextBusToRoute(routeID,direction):
     bus = getAvaliableBus()
-    bus.setRouteID(routeID)
-    bus.setStatus(direction)
+    bus.set_route_id(routeID)
+    bus.set_status(direction)
 
 #sets every direction of every route to have a bus
 def startOfDay():
@@ -43,62 +43,62 @@ while True:
              
         if  randint(0,route_amount*2) == 0:#random check
             
-            if bus.getStatus() == "A-B":#checks direction 
+            if bus.get_status() == "A-B":#checks direction
 
                 
-                currentRoute = json_passing.get_route(bus.getRouteID())#retrives route information
+                currentRoute = json_passing.get_route(bus.get_route_id())#retrives route information
 
-                charge = bus.getCharge() - currentRoute["A-B"]#new charges
-                bus.setCharge(charge)#updates charge
+                charge = bus.get_charge() - currentRoute["A-B"]#new charges
+                bus.set_charge(charge)#updates charge
 
 
-                if bus.getEndOfJourney():#checks if bus should go back to depot
-                    bus.endOfJourneyFalse()
-                    bus.setStatus("B-Depot")
+                if bus.get_end_of_journey():#checks if bus should go back to depot
+                    bus.end_of_journey_false()
+                    bus.set_status("B-Depot")
                 elif currentRoute["A-B"]+currentRoute["B-A"]+currentRoute["B-Depot"] < charge:#checks if bus should start journey
-                    bus.endOfJourneyTrue() # fix wrong direction 
-                    bus.setStatus("B-A")
+                    bus.end_of_journey_true() # fix wrong direction
+                    bus.set_status("B-A")
                     nextBus = getAvaliableBus()#gets new bus to replace previous
                     
-                    nextBus.setRouteID(bus.getRouteID())
-                    nextBus.setStatus("depot-A")
+                    nextBus.set_route_id(bus.get_route_id())
+                    nextBus.set_status("depot-A")
 
                    
                 else:
-                    bus.setStatus("B-A")
+                    bus.set_status("B-A")
                     json_passing.update_bus(i,"status","B-A")#if neither is true tells bus to continue
                     
-            elif bus.getStatus() == "B-A":#repeat as above but other direction
-                currentRoute = json_passing.get_route(bus.getRouteID())#retrives route information
+            elif bus.get_status() == "B-A":#repeat as above but other direction
+                currentRoute = json_passing.get_route(bus.get_route_id())#retrives route information
 
-                charge = bus.getCharge() - currentRoute["B-A"]
-                bus.setCharge(charge)
+                charge = bus.get_charge() - currentRoute["B-A"]
+                bus.set_charge(charge)
                 
-                if bus.getEndOfJourney():
+                if bus.get_end_of_journey():
 
-                    bus.endOfJourneyFalse()
-                    bus.setStatus("A-Depot")
+                    bus.end_of_journey_false()
+                    bus.set_status("A-Depot")
 
                 elif currentRoute["B-A"]+currentRoute["A-B"]+currentRoute["A-Depot"] < charge:
                     
-                    bus.endOfJourneyTrue()
-                    bus.setStatus("A-B")
+                    bus.end_of_journey_true()
+                    bus.set_status("A-B")
                     
 
                     nextBus = getAvaliableBus()
-                    nextBus.setRouteID(bus.getRouteID())
-                    nextBus.setStatus("depot-A")
+                    nextBus.set_route_id(bus.get_route_id())
+                    nextBus.set_status("depot-A")
 
                 else:
-                    bus.setStatus("A-B")
+                    bus.set_status("A-B")
             
-            elif bus.getStatus() == "depot-A":#checks if bus has been driving from depot to route, if true tells to begin route
-                bus.setStatus("A-B")
-            elif bus.getStatus() == "depot-B":#repeat for other direction
-                bus.getStatus("A-B")
+            elif bus.get_status() == "depot-A":#checks if bus has been driving from depot to route, if true tells to begin route
+                bus.set_status("A-B")
+            elif bus.get_status() == "depot-B":#repeat for other direction
+                bus.get_status("A-B")
         
-            elif bus.getStatus() ==  "A-Depot" or bus.getStatus() == "B-Depot":#checks if bus has just entered depot
-                bus.setStatus("queueing")
+            elif bus.get_status() == "A-Depot" or bus.get_status() == "B-Depot":#checks if bus has just entered depot
+                bus.set_status("queueing")
                 chargeQueue.append(bus)#adds to charging queue
 
               
@@ -110,12 +110,12 @@ while True:
             charger.reduceChargeTime()
             if charger.getChargeTime() == 0:
                 outbus = charger.getBus()
-                outbus.setStatus("parked")
-                outbus.setCharge(100)
+                outbus.set_status("parked")
+                outbus.set_charge(100)
                 charger.setBus(None)
         if charger.getBus() == None and chargeQueue:
             inbus = chargeQueue.pop()
-            inbus.setStatus("Charging")
+            inbus.set_status("Charging")
             charger.setBus(inbus)
             charger.setChargeTime(5)
 
