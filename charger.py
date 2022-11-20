8356c8cdf788621c2fdf816e232b2cfbe77cbc16
Colin <108523746@umail.ucc.ie>
2022-11-20 14:49:10 +0000

    Refactored Charger class

--- charger.py
+++ charger.py
@@ -1,20 +1,19 @@
 class Charger:
     def __init__(self):
         self.bus = None
-        self.chargetime = None
-        
+        self.charge_time = None
 
-    def getBus(self):
+    def get_bus(self):
         return self.bus
-    
-    def setBus(self, bus):
-        self.bus = bus
-        
-    def getChargeTime(self):
-        return self.chargetime
-    
-    def setChargeTime(self, chargetime):
-        self.chargetime = chargetime
 
-    def reduceChargeTime(self):
-        self.chargetime +=1
+    def set_bus(self, bus):
+        self.bus = bus
+
+    def get_charge_time(self):
+        return self.charge_time
+
+    def set_charge_time(self, charge_time):
+        self.charge_time = charge_time
+
+    def reduce_charge_time(self):
+        self.charge_time += 1
