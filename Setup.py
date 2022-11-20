67a617ced8451b161717bdf08ce7a1ec92fc2d9f
Colin <108523746@umail.ucc.ie>
2022-11-20 16:44:04 +0000

    Renamed setup to Setup class. Also refactored

---
+++ Setup.py
@@ -0,0 +1,22 @@
+import Parser
+from random import randint
+from Bus import Bus
+
+
+def setup_bus(num_buses):
+    out_list = []
+    for i in range(num_buses):
+        out_list.append(Bus(i))
+    return out_list
+
+
+def setup_random_routes(num_routes):
+    out_dict = {}
+    for i in range(num_routes):
+        temp = {"A-B": randint(5, 20), "B-A": randint(5, 20), "A-Depot": randint(0, 15), "B-Depot": randint(0, 15)}
+        route_id = i
+        out_dict[route_id] = temp
+    json_passing.export_routes(out_dict)
+
+
+setup_random_routes(1)
