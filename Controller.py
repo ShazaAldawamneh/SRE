0c3dd349e7fa11c4405bc64d0e58269b9fe67131
Colin <108523746@umail.ucc.ie>
2022-12-05 22:04:20 +0000

    Refactored Controller class.

--- Controller.py
+++ Controller.py
@@ -1,7 +1,6 @@
 import Setup
 from ChargingStation import ChargingStation
 import Routes
-from random import randint
 from Queue import Queue
 
 
@@ -56,7 +55,7 @@ class Controller:
                 # charger.set_charge_time == None
         if charger.get_bus() is None and not self.charge_queue.empty():
             in_bus = self.charge_queue.dequeue()
-            print(self.charge_queue)
+            # (self.charge_queue)
             in_bus.set_status("CHARGING")
             charger.set_bus(in_bus)
             charger.set_charge_time(5)
@@ -66,63 +65,67 @@ class Controller:
         for charge_time in range(1, 6):
             for charger in self.chargers:
                 if charger.get_charge_time() == charge_time:
-                    print(charger.get_charge_time())
                     out_bus = charger.get_bus()
                     out_bus.set_charge(100 - (20 * charge_time))
                     charger.set_bus(None)
                     charger.set_charge_time(None)
                     return out_bus
 
-    def check(self, bus):
-        
+    def bus_manager(self, bus):
+        """TODO"""
         if bus.get_status() == "A-B":  # checks direction
             current_route = Routes.get_route(
-            bus.get_route_id())  # retrieves route information
-            # performs the operation here
+                bus.get_route_id())  # retrieves route information
             bus.subtract_charge(current_route["A-B"])
             charge = bus.get_charge()
+
             if bus.get_end_of_journey():  # checks if bus should go back to depot
                 bus.set_end_of_journey(False)
                 bus.set_status("B-Depot")
+
             elif current_route["A-B"] + current_route["B-A"] + \
-                current_route["B-Depot"] > charge:  # checks if bus should start journey
+                    current_route["B-Depot"] > charge:  # checks if bus should start journey
                 bus.set_end_of_journey(True)
                 bus.set_status("B-A")
                 next_bus = self.get_available_bus()  # gets new bus to replace previous
+
                 if next_bus is None:
                     raise Exception("No buses available")
                 next_bus.set_route_id(bus.get_route_id())
                 next_bus.set_status("Depot-A")
+
             else:
                 bus.set_status("B-A")
+
         elif bus.get_status() == "B-A":  # repeat as above but other direction
             current_route = Routes.get_route(bus.get_route_id())  # retrieves route information
             charge = bus.get_charge() - current_route["B-A"]
             bus.set_charge(charge)
+
             if bus.get_end_of_journey():
                 bus.set_end_of_journey(False)
                 bus.set_status("A-Depot")
+
             elif current_route["B-A"] + current_route["A-B"] + current_route["A-Depot"] > charge:
                 bus.set_end_of_journey(True)
                 bus.set_status("A-B")
                 next_bus = self.get_available_bus()
+
                 if next_bus is None:
                     raise Exception("No buses available")
                 next_bus.set_route_id(bus.get_route_id())
                 next_bus.set_status("Depot-B")
+
             else:
                 bus.set_status("A-B")
         elif bus.get_status() == "Depot-A":  # checks if bus has been driving from depot to route
             bus.set_status("A-B")
+
         elif bus.get_status() == "Depot-B":  # repeat for other direction
             bus.set_status("A-B")
+
         elif bus.get_status() == "A-Depot" or bus.get_status() == "B-Depot":  # checks if bus has just
             # entered depot
             bus.set_status("QUEUED")
             self.charge_queue.enqueue(bus)  # adds to charging queue'
             bus.set_route_id(None)
-            print(self.charge_queue)
-                    
-
-
-
