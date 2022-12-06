e90285c1bea16e2d4db49284358520140cd0b428
Rheena Blas <73996643+rheenablas@users.noreply.github.com>
2022-12-06 16:07:57 +0000

    Added docstrings in Simulation.py

--- Simulation.py
+++ Simulation.py
@@ -2,7 +2,7 @@ from random import randint
 
 
 class Simulation:
-    """TODO"""
+    """A simulation of the bus-charging algorithm."""
 
     def loop(self):
         """ Method for looping through each bus and checking if it has reached the end of its journey. """
@@ -15,7 +15,7 @@ class Simulation:
             self.charger_dequeue(charger)
 
     def sim_on_click(self, max_loops):
-        """TODO"""
+        """Starts initialisation of the bus and ends on the given argument (max_loops)."""
         count = 0
         self.start_of_day()
         while count < max_loops:
